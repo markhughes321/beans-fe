cbf9df1b6f9ed9cffb8b063ebc804e849c4c4088
Mark Hughes <m.hughes@heni.com>
2023-04-14 10:21:53 +0100

    improved articleCard styling and added new font

--- src/components/ArticleCard/index.js
+++ src/components/ArticleCard/index.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 function ArticleCard({ article }) {
-  const { articleSource, articleTitle, articleImageUrl, articleCreatedDate, articleIntro } = article;
+  const {
+    articleSource,
+    articleTitle,
+    articleImageUrl,
+    articleCreatedDate,
+    articleIntro,
+  } = article;
 
   return (
-    <a className="article-link" href={article.articleUrl} target="_blank" rel="noopener noreferrer">
-      <div className="article-card">
-        <div className="article-image-container">
-          <img className="article-image" src={articleImageUrl} alt={articleTitle} />
-        </div>
-        <div className="article-content">
-          <div className="article-domain">{articleSource}</div>
+    <div className="article-card">
+      <div className="article-content">
+        <a
+          className="article-link"
+          href={article.articleUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="article-domain">{articleSource}</p>
           <h2 className="article-title">{articleTitle}</h2>
-          <p className="article-description">{articleIntro}</p>
-          <span className="article-date">{articleCreatedDate}</span>
-        </div>
+          <div className="article-image-container">
+            <img
+              className="article-image"
+              src={articleImageUrl}
+              alt={articleTitle}
+            />
+          </div>
+            <p className="article-date">{articleCreatedDate}</p>
+            <p className="article-description">{articleIntro}</p>
+        </a>
       </div>
-    </a>
+    </div>
   );
 }
 
